96dbff296d392d7c0d8f30cf4c7dc6f8901d9249
kayrus <kay.diam@gmail.com>
2021-11-09 13:36:28 +0100

    Add keystone load test support

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync/atomic"
+	"time"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
@@ -17,9 +19,11 @@ func main() {
 	ao := &ec2tokens.AuthOptions{}
 	var authURL string
 	var debug bool
+	var threads uint
 	flag.StringVar(&authURL, "auth-url", "", "Keystone auth URL")
 	flag.StringVar(&ao.Access, "access", "", "EC2 access")
 	flag.StringVar(&ao.Secret, "secret", "", "EC2 secret")
+	flag.UintVar(&threads, "threads", 0, "Whether to run an infinite loop with an amount of threads")
 	flag.BoolVar(&debug, "debug", false, "show debug logs")
 	flag.Parse()
 
@@ -74,15 +78,55 @@ func main() {
 		log.Fatal(err)
 	}
 
-	res, err := pkg.OpenStackEC2Auth(identityClient, ao)
-	if err != nil {
-		log.Fatal(err)
+	fps := new(uint64)
+	ops := new(uint64)
+	auth := func(limiter chan struct{}) {
+		atomic.AddUint64(ops, 1)
+		res, err := pkg.OpenStackEC2Auth(identityClient, ao)
+		if err != nil {
+			atomic.AddUint64(fps, 1)
+			if limiter == nil {
+				log.Print(err)
+				os.Exit(1)
+			}
+		}
+
+		if debug {
+			log.Printf("User: %s", res.Username)
+			log.Printf("Project: %s", res.Project)
+		}
+
+		if limiter == nil {
+			fmt.Println(res.TokenID)
+			return
+		}
+
+		<-limiter
 	}
 
-	if debug {
-		log.Printf("User: %s", res.Username)
-		log.Printf("Project: %s", res.Project)
+	if threads == 0 {
+		auth(nil)
+		os.Exit(0)
 	}
 
-	fmt.Println(res.TokenID)
+	go func() {
+		for {
+			select {
+			case <-time.After(1 * time.Second):
+				f := atomic.SwapUint64(fps, 0)
+				s := atomic.SwapUint64(ops, 0)
+				var perc uint64
+				if s > 0 {
+					perc = 100 * f / s
+				}
+				log.Printf("%d rps, %d failed (%d%%)", s, f, perc)
+			}
+		}
+	}()
+
+	limiter := make(chan struct{}, threads)
+	for {
+		limiter <- struct{}{}
+		go auth(limiter)
+	}
 }
